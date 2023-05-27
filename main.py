24b2d1a6c33665e85219b1dd0bc5930f18f4e0a3
expense <myexpense09@gmail.com>
2023-05-27 16:12:07 +0500

    second commit

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from userrights import admin_only
+import os
 
 # IS_ADMIN = False
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+app.config['SECRET_KEY'] = os.getenv('key_scrt')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
